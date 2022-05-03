db9dcf4a0e236ab2fe8b0cdf8cf7d8b2857a3554
Katherine Jiang <katherine.jiang30@gmail.com>
2022-05-03 17:44:36 -0400

    completed twoSum challenge

---
+++ twoSum.py
@@ -0,0 +1,32 @@
+# import testmod for testing our function
+from doctest import testmod
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    """Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+    >>> twoSum([2,7,11,15], 9)
+    [0, 1]
+    >>> twoSum([3,3], 6)
+    [0, 1]
+    """
+    sum_dict={} # {num:index}
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in sum_dict:
+            return [sum_dict.get(diff), index]
+        sum_dict[num] = index
+    else:
+        return []
+
+#Runtime complexity: O(n)
+#Spacetime complexity:O(n)
+
+
+# call the testmod function
+if __name__ == '__main__':
+    testmod(name ='twoSum', verbose = True)
