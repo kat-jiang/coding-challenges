b8e88f9d336695198c2fc163c1218d66139770cc
Katherine Jiang <katherine.jiang30@gmail.com>
2022-05-04 00:54:35 -0400

    completed contains-duplicate.py with testing

---
+++ contains-duplicate.py
@@ -0,0 +1,27 @@
+# import testmod for testing our function
+from doctest import testmod
+from typing import List
+
+def contains_duplicate(nums: List[int]) -> bool:
+    """Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+    >>> contains_duplicate([1,2,3,1])
+    True
+    >>> contains_duplicate([1,2,3,4])
+    False
+    >>> contains_duplicate([1,1,1,3,3,4,3,2,4,2])
+    True
+    """
+
+    duplicate_dict = {}
+
+    for num in nums:
+        if duplicate_dict.get(num):
+            return True
+        duplicate_dict[num] = duplicate_dict.get(num, 0) + 1
+
+    return False
+
+# call the testmod function
+if __name__ == '__main__':
+    testmod(name ='max_profit', verbose = True)
