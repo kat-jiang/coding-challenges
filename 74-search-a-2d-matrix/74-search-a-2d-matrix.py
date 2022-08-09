e9d8af2d2d971de693dc93fcb807e5ea33e1f19b
Kat Jiang <97914730+kat-jiang@users.noreply.github.com>
2022-08-09 16:18:35 -0400

    Time: 69 ms (52.37%), Space: 14.4 MB (42.97%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,12 +1,41 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #binary search
+        #get len of rows and cols
+        #get mid point 
+        #work on rows first
+        #if target > last num in row, increase row
+        #if target < first num in row, decrease row
+        #else target in row
+        #work on cols next, l and r
+        #get mid point - if target > mid, increment l
+        #if target < mid, decrease r
         
-        for row in matrix:
-            if row[0] <= target and row[-1] >= target:
-                for i in row:
-                    if i == target:
-                        return True
-                    elif i > target:
-                        return False
-            elif row[0] > target:
-                return False
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        top, bottom = 0, ROWS - 1
+        
+        while top <= bottom:
+            mid = (top + bottom) // 2
+            if target > matrix[mid][-1]:
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bottom =  mid - 1
+            else:
+                break
+        #while loop breaks naturally meaning target not within any of the rows
+        else:
+            return False
+        
+        l, r = 0, COLS - 1
+        cur_row = (top + bottom) // 2
+        while l <= r:
+            mid = (l + r) //2
+            if target > matrix[cur_row][mid]:
+                l = mid + 1
+            elif target < matrix[cur_row][mid]:
+                r = mid - 1
+            else:
+                return True
+            
+        return False
