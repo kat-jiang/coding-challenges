33fc372e29775392cc9d2e12edd2c93b49568758
Kat Jiang <katherine.jiang30@gmail.com>
2025-01-14 17:33:55 -0500

    update #238 product of array except self

--- arrays/238-product-of-array-except-self/238-product-of-array-except-self.py
+++ arrays/238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,15 +1,20 @@
+# since we cannot use division, we can calculate the product via 
+# the product of all elements to the left and right of the current element
+# create a return array of the same length as the input array, consisting of 1s
+# iterate through the array, storing the product of all elements to the right of the current element
+# iterate through the array in reverse, storing the product of all elements to the left of the current element
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * len(nums)
-        
-        prefix = 1
+
+        right_side = 1
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-            
-        postfix = 1
+            res[i] = right_side
+            right_side *= nums[i]
+
+        left_side = 1
         for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-            
+            res[i] *= left_side
+            left_side *= nums[i]
+
         return res
