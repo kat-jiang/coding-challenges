44af1ab6da773fd35b6ecbc9223393e4f763e665
Kat Jiang <katherine.jiang30@gmail.com>
2025-01-15 13:09:41 -0500

    update #125 valid palindrone

--- two-pointer/125-valid-palindrome/125-valid-palindrome.py
+++ two-pointer/125-valid-palindrome/125-valid-palindrome.py
@@ -1,5 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        #convert to lowercase
+        #remove spaces and non-alphanumeric chars
+        #reverse the string and compare
+        lowercase_string = s.lower()
+        converted_string = ""
+        for char in lowercase_string:
+            if char.isalnum():
+                converted_string += char
+
+        return converted_string == converted_string[::-1]
+
+# Time complexity: O(n)
+
+# Two pointer approach
+
+# convert to lowercase
+# remove spaces and non-alphanumeric chars
+# compare the first and last char using 2 pointers
+# if they are not equal, return False
+# increment left pointer and decrement right pointer
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s = "".join([char for char in s if char.isalnum()])
         l = 0
