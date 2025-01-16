aa2cbdf5eb71b83be73329dd5ae5551db398b505
Kat Jiang <katherine.jiang30@gmail.com>
2025-01-16 14:18:21 -0500

    update to sliding window problems

--- sliding window/3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ sliding window/3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,15 +1,22 @@
+# use 2 pointers left and right pointer to iterate through the string
+# variable to save longest string count
+# set to store seen letters
+# if seen before, increment the left pointer, clear the set
+# if letter not seen before, increment the right pointer
+# ends when right pointer reaches end of string
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        substring = set()
-        l = 0
+        seen = set()
+        left_p = 0
         max_length = 0
-        
-        for i in range(len(s)):
-            while s[i] in substring:
-                substring.remove(s[l])
-                l += 1
-            substring.add(s[i])
-            max_length =  max(max_length, len(substring))
+
+        for right_p in range(len(s)):
+            while s[right_p] in seen:
+                seen.remove(s[left_p])
+                left_p += 1
+            seen.add(s[right_p])
+            max_length =  max(max_length, len(seen))
         
         return max_length
         
--- sliding window/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ sliding window/424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,3 +1,10 @@
+# use 2 pointers for sliding window
+# left pointer starts at 0 and right pointer will iterate through the array
+# create a dictionary to store the count of each character
+# iterate through the array and count the characters
+# we can return longest repeating char string by getting the size of the window
+# if the window size - max(count.values()) > k, we need to shrink the window
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {} #char:count
