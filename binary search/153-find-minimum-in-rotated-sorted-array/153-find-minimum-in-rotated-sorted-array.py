c9d4b227c0db17b9e31ff3ddc393ed8409d7c01f
Kat Jiang <katherine.jiang30@gmail.com>
2025-02-10 15:17:23 -0500

    add comments and pseudocode

--- binary search/153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ binary search/153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,21 +1,28 @@
+
+# Approach:
+# Identify the property of the rotated sorted array:
+#   The minimum element is the pivot point where the rotation occurred.
+#   The rightmost element is always greater than or equal to the minimum.
+# Binary Search Strategy:
+#   Set left = 0 and right = len(nums) - 1.
+#   Find mid = (left + right) // 2.
+#   If nums[mid] > nums[right], it means the minimum must be in the right half (move left = mid + 1).
+#   Otherwise, the minimum is either at mid or in the left half (move right = mid).
+#   Continue this process until left == right, which will point to the minimum element.
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        lowest = nums[0]
+        left, right = 0, len(nums) - 1
         
-        while l <= r:
-            if nums[l] < nums[r]:
-                lowest = min(lowest, nums[l])
-                return lowest
+        while left < right:
+            mid = (left + right) // 2
             
-            mid = (l + r) // 2
-            lowest = min(lowest, nums[mid])
-            
-            if nums[mid] >= nums[l]:
-                l =  mid + 1
-            else:
-                r = mid - 1
+            if nums[mid] > nums[right]:  # Min must be in the right half
+                left = mid + 1
+            else:  # Min is at mid or in the left half
+                right = mid
+        
+        return nums[left]  # or nums[right], since left == right at the end
                 
-        return lowest
-                
-                
+# Time Complexity Analysis:
+# O(log n) since we halve the search space in each step.
+# O(1) space since we use only a few variables.
