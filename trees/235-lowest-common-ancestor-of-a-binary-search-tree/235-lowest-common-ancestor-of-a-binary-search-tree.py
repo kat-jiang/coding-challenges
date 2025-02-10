c489786c3d3c246d57e37782cb7b822e97afce27
Kat Jiang <katherine.jiang30@gmail.com>
2025-02-10 18:02:23 -0500

    add comments

--- trees/235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ trees/235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,11 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
         while root:
+            # If both p and q are greater than root, LCA is in the right subtree
             if p.val > root.val and q.val > root.val:
                 root = root.right
+            # If both p and q are less than root, LCA is in the left subtree
             elif p.val < root.val and q.val < root.val:
-                root =  root.left
+                root = root.left
             else:
+                # If p and q are on different sides (or one is root itself), root is the LCA
                 return root
