e38a95bee444a81c08bb8c0b77d0594203529fe4
Kat Jiang <katherine.jiang30@gmail.com>
2025-02-20 12:44:29 -0500

    add pseudocode

--- linked-lists/141-linked-list-cycle/141-linked-list-cycle.py
+++ linked-lists/141-linked-list-cycle/141-linked-list-cycle.py
@@ -18,3 +18,22 @@ class Solution:
                 return True
 
         return False
+
+# Floyd's Cycle Detection Algorithm
+def detectLoop(head):
+  
+    # Fast and slow pointers initially points to the head
+    slow = head
+    fast = head
+
+    # Loop that runs while fast and slow pointer are not
+    # None and not equal
+    while slow and fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        # If fast and slow pointer points to the same node,
+        # then the cycle is detected
+        if slow == fast:
+            return True
+    return False
